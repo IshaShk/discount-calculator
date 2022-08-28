65cf048caa67aa9d153a883ddb6ad8cf34e326b7
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2022-08-28 13:56:21 +0500

    Added EventListener

---
+++ script.js
@@ -0,0 +1,9 @@
+const calculateBtn = document.querySelector('.calculate');
+const resetBtn = document.querySelector('.reset');
+
+calculateBtn.addEventListener('click' , (e) => {
+    let bill = document.getElementById('amount').value;
+    let percentage = document.getElementById('discount').value;
+    let discountAmt = document.getElementById('discount-amount');
+    let finalPay = document.getElementById('pay');
+})
