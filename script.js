c2546524269361d744e6d2deab48fa93b9d86902
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2022-08-28 14:00:49 +0500

    calculate discount

--- script.js
+++ script.js
@@ -6,4 +6,8 @@ calculateBtn.addEventListener('click' , (e) => {
     let percentage = document.getElementById('discount').value;
     let discountAmt = document.getElementById('discount-amount');
     let finalPay = document.getElementById('pay');
+
+
+    discountAmt.value = bill * percentage/100;
+    finalPay.value = bill - discountAmt;
 })
